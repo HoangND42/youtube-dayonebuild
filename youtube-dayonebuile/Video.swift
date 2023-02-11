418e0859711459e9c5ac705f8b00f71b7ee88923
HoangND42 <HoangND42@gmail.com>
2023-02-12 00:37:41 +0700

    added api file

---
+++ youtube-dayonebuile/Video.swift
@@ -0,0 +1,17 @@
+//
+//  Video.swift
+//  youtube-dayonebuile
+//
+//  Created by apple on 2/10/23.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+}
